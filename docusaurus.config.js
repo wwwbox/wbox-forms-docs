a20516f1678677e851f03accf689632ea563e48c
Ali Faris <alihcompiler@gmail.com>
2022-03-17 23:58:23 +0300

    changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,9 +22,10 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          editCurrentVersion: false,
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+          editUrl: 'https://github.com/alicompiler/wbox-forms-docs',
         },
         blog: {
           showReadingTime: true,
--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -5,42 +5,37 @@ import styles from './HomepageFeatures.module.css';
 const FeatureList = [
   {
     title: 'Easy to Use',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        With wBox-Forms you can create fully functional forms in minutes
       </>
     ),
   },
   {
     title: 'Focus on What Matters',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        wBox-Forms lets you focus on what matter rather than spending time on writing boilerplate code
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
+    title: 'Customizable',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        With wBox-Forms, you can always change how things are working.
       </>
     ),
   },
+  {
+    title: 'Extensible',
+    description: 'Also, this library come with set of plugins to help you write less code'
+  }
 ];
 
 function Feature({Svg, title, description}) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} alt={title} />
-      </div>
+    <div className={clsx('col col--3')}>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
         <p>{description}</p>
